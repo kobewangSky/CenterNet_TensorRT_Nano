a7cb218ce31b137f83c37aee8528007a447c35a1
kobe <bluce54088@gmail.com>
2019-08-30 19:11:25 +0800

    if exp_id is empty assign backbone name

--- utils/opts.py
+++ utils/opts.py
@@ -206,6 +206,8 @@ class opts(object):
         opt.root_dir = os.path.join(os.path.dirname(__file__), '..')
         opt.data_dir = os.path.join(opt.root_dir, 'data')
         opt.exp_dir = os.path.join(opt.root_dir, 'exp', opt.task)
+        if opt.exp_id == 'default':
+            opt.exp_id = opt.backbone
         opt.save_dir = os.path.join(opt.exp_dir, opt.exp_id)
         opt.debug_dir = os.path.join(opt.save_dir, 'debug')
         print('The output will be saved to ', opt.save_dir)
