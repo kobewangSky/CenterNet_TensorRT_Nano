5f0725aeed141741c5ffaa7013ac2402927952d9
kobe <bluce54088@gmial.com>
2019-11-18 20:41:17 +0800

    .docker_tensorrt_python_x86_x64/Dockerfile add cocoapi

--- torch2trt.py
+++ torch2trt.py
@@ -1,51 +1,66 @@
 import torch
 from torch2trt import torch2trt
-from torchvision.models.resnet import resnet152
+from torchvision.models.resnet import resnet50
 import time
-
+import numpy as np
 # create some regular pytorch model...
-model = resnet152(pretrained=True).eval().cuda()
+model = resnet50(pretrained=True).eval().cuda()
 
 # create example data
 x = torch.ones((1, 3, 512, 512)).cuda()
 
+print(sum(p.numel() for p in model.parameters() if p.requires_grad))
+
 # convert to TensorRT feeding sample data as input
-model_trt = torch2trt(model, [x])
-model_trt_16 = torch2trt(model, [x], fp16_mode=True)
-model_trt_8 = torch2trt(model, [x], int8_mode=True)
+print('x = torch.ones((1, 3, 512, 512)).cuda()')
+#model_trt = torch2trt(model, [x])
+# model_trt_16 = torch2trt(model, [x], fp16_mode=True)
+# model_trt_8 = torch2trt(model, [x], int8_mode=True)
+timelist = []
+print('y = model(x)')
+for i in range(101):
 
-start = time.time()
-y = model(x)
-end = time.time()
-print(end - start)
+    start = time.time()
+    y = model(x)
+    end = time.time()
+    print(end - start)
+    timelist.append(end - start)
+temp = np.array(timelist)
+print(sum(timelist))
+print('mean = {}'.format(temp.mean()))
 
-
-start = time.time()
-y_trt = model_trt(x)
-end = time.time()
-print(end - start)
-
-start = time.time()
-y_trt = model_trt_16(x)
-end = time.time()
-print(end - start)
-
-start = time.time()
-y_trt = model_trt_8(x)
-end = time.time()
-print(end - start)
-
-# torch.save(model_trt.state_dict(), 'alexnet_trt.pth')
-#
-# from torch2trt import TRTModule
-#
-# model_trt = TRTModule()
-#
-# model_trt.load_state_dict(torch.load('alexnet_trt.pth'))
-#
+# print('y_trt = model_trt(x)')
 # start = time.time()
 # y_trt = model_trt(x)
 # end = time.time()
 # print(end - start)
 #
-# print(1)
+# start = time.time()
+# y_trt = model_trt_16(x)
+# end = time.time()
+# print(end - start)
+
+# start = time.time()
+# y_trt = model_trt_8(x)
+# end = time.time()
+# print(end - start)
+
+#torch.save(model_trt.state_dict(), 'alexnet_trt.pth')
+
+from torch2trt import TRTModule
+
+model_trt_load = TRTModule()
+
+model_trt_load.load_state_dict(torch.load('Resnet_50.pth'))
+timelist.clear()
+timelist = []
+for i in range(101):
+    start = time.time()
+    y_trt = model_trt_load(x)
+    end = time.time()
+    print(end - start)
+    timelist.append(end - start)
+temp = np.array(timelist)
+print(sum(timelist))
+print('mean = {}'.format(temp.mean()))
+print(1)
